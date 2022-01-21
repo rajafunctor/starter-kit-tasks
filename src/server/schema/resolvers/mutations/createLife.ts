cf573a4a974f05766d097ab8f05948843913c886
rajafunctor <raja@appvantage.asia>
2022-01-21 10:46:14 +0530

    feat(graphql): added createlife mutations

---
+++ src/server/schema/resolvers/mutations/createLife.ts
@@ -0,0 +1,28 @@
+import { ObjectId } from 'mongodb';
+import { getDatabaseContext, Life } from '../../../database';
+import { GraphQLMutationResolvers } from '../definitions';
+
+const mutation: GraphQLMutationResolvers['createLife'] = async (
+    root,
+    { firstName, lastName, title, birthday, hobbies, description }
+) => {
+    const { collections } = await getDatabaseContext();
+
+    const lifeId = new ObjectId();
+
+    const document: Life = {
+        _id: lifeId,
+        title,
+        firstName,
+        lastName,
+        description,
+        hobbies,
+        birthday,
+    };
+
+    await collections.lives.insertOne(document);
+
+    return { ...document, id: document._id, fullName: `${firstName} ${lastName}` };
+};
+
+export default mutation;
